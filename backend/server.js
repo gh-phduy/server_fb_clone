6ff046c2b5d1256ac314650cc60354c92db0608b
Phuong Duy <evnnpd@gmail.com>
2024-06-05 00:52:26 +0700

    Push Day 5

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ import express from "express"
 import dotenv from "dotenv"
 import connectMongoDB from "./db/connect_mongo.js"
 import { v2 as cloudinary } from "cloudinary";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routers/auth_router.js"
 import userRoutes from "./routers/user_router.js";
 import postRoutes from "./routers/post_router.js";
 import notificationRoutes from "./routers/notify_router.js";
+import cors from "cors"
 
 dotenv.config()
 
@@ -19,14 +20,17 @@ cloudinary.config({
 
 const __dirname = path.resolve();
 
+
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
-
-const app = express()
-const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
