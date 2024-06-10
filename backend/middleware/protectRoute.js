1372c92660ed603e0c22477a014bbd342a26038f
Phuong Duy <evnnpd@gmail.com>
2024-06-10 17:01:32 +0700

    Push 2

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 export const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
+		console.log(req.cookies)
 		if (!token) {
 			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
 		}
--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,14 @@ const __dirname = path.resolve();
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+	origin: "http://localhost:3000",
+	credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
-
-app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
